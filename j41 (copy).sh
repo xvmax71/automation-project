05feff6209e09b9ced604f2f0d34a898ba98a918
mushu71 <musharrafmohd638@gmail.com>
2023-02-03 20:04:28 +0530

    done

--- j41 (copy).sh
+++ j41 (copy).sh
@@ -40,17 +40,63 @@ fi
 for file in /home/mushu/Downloads/*.aac; do
 
 
-  ffmpeg -i "$file" -acodec mp3 "${file%.aac}.mp3"
+  ffmpeg -i "$file" -acodec mp3 "${file%.aac}.mp3" -y
+
 
 
   tar cvfz "${file%.aac}.tgz" "${file%.aac}.mp3"
 
 
 
-  aws s3 cp "${file%.aac}.tgz" s3://maximum1
 
-  sleep 3
 done
+
+
+for file in /home/mushu/Downloads/*.mp3; do
+  rm "$file"
+done
+
+
+for file in /home/mushu/Downloads/*.tgz; do
+ filename="$(basename "$file")"
+
+ if [[ "$filename" == saad* ]]; then
+    dirname="saad_s"
+  elif [[ "$filename" == miran* ]]; then
+    dirname="miran_s"
+  elif [[ "$filename" == mushu* ]]; then
+    dirname="mushu_s"
+  elif [[ "$filename" == talaiva* ]]; then
+    dirname="talaiva_s"
+  elif [[ "$filename" == dalla* ]]; then
+    dirname="dalla_s"
+  elif [[ "$filename" == tauseef* ]]; then
+    dirname="tauseef_s"
+  fi
+
+    dirname="/home/mushu/Downloads/$dirname"
+
+  if [ ! -d "$dirname" ]; then
+
+    mkdir "$dirname"
+  fi
+
+  mv "$file" "$dirname/"
+done
+
+new_dir="/home/mushu/Downloads/finaldata"
+
+if [ ! -d "$new_dir" ]; then
+mkdir -p "$new_dir"
+fi
+
+for dir in /home/mushu/Downloads/saad_s /home/mushu/Downloads/miran_s /home/mushu/Downloads/mushu_s /home/mushu/Downloads/talaiva_s /home/mushu/Downloads/dalla_s /home/mushu/Downloads/tauseef_s; do
+mv "$dir" "$new_dir/"
+done
+
+
+aws s3 sync /home/mushu/Downloads/finaldata/  s3://maximum1
+
 
 SUBJECT="the automation task is complete"
 MESSAGE="the given automation task is successfully complete"
@@ -63,3 +109,4 @@ PASSWORD="xowramddsbvpqbxh"
 echo -e "subject: $SUBJECT\n\n\$MESSAGE" | \
 echo -e "To: $TO\nFROM: $FROM\nSubject: $SUBJECT\n\n$MESSAGE" | \
 ssmtp -au $USERNAME -ap $PASSWORD $TO
+
